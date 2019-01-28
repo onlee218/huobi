ee3fb857057c646c5942fd782cf24e0d37899c2c
lei huang <onlee@163.com>
2019-01-28 20:52:37 +0800

    333

---
+++ Huobi.Rest.CSharp.Demo/Model/Balance.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Huobi.Rest.CSharp.Demo.Model
+{
+    public class Balance
+    {
+        public long Id { get; set; }
+        public string Type { get; set; }
+        public string State { get; set; }
+        public Array List { get; set; }
+    }
+}
